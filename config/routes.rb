f23e015ec2e803dc3fe4282131cdeffd0f3a36a8
joostcalis <joost_calis@hotmail.com>
2016-02-06 13:05:16 +0100

    serverprobleem opgelost

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'post/new' => 'post#new'
+  root 'post#new'
 
 
   # root 'welcome#index'
